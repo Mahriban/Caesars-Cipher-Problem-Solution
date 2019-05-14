046973d8f8f48e494dc2d35fd53a4ed5020d9010
Mahriban <42981721+Mahriban@users.noreply.github.com>
2019-05-14 10:59:59 +0200

    Problem Solution

---
+++ caesarsCipher.js
@@ -0,0 +1,18 @@
+function rot13(str) { // LBH QVQ VG!
+    let newArr = [];
+    let alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz ?!".split("");
+    let rot13 = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm ?!".split("");
+    let strArr=  str.split("");
+    
+    for (let i= 0; i<alphabet.length; i++){
+      if (alphabet.indexOf(strArr[i]) != -1){
+        newArr.push(rot13[alphabet.indexOf(strArr[i])])
+      }
+    }
+    
+      return newArr.join("")
+    }
+    
+    
+    // Change the inputs below to test
+    rot13("SERR CVMMN!");
